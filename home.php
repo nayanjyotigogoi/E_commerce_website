696bc45ed0895c1104bdaec945b0863c607b3625
Nayanjyoti Gogoi <90410611+nayanjyotigogoi@users.noreply.github.com>
2023-06-16 18:58:33 +0530

    Add files via upload
    First commit

---
+++ home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Clothing Store</title>
+  <link rel="stylesheet" href="home.css">
+</head>
+<body>
+  <header>
+    <div class="container">
+      <h1>Welcome to My Clothing Store</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <section class="hero">
+    <div class="container">
+      <h2>Discover the Latest Trends in Clothing</h2>
+      <p>Shop our wide range of stylish clothing for men, women, and kids.</p>
+      <a href="#" class="btn">Shop Now</a>
+    </div>
+  </section>
+
+  <section class="featured-products">
+    <div class="container">
+      <h2>Featured Products</h2>
+      <div class="product-grid">
+        <div class="product-item">
+          <img src="product1.jpg" alt="Product 1">
+          <h3>Product 1</h3>
+          <p>$49.99</p>
+          <a href="#" class="btn">Add to Cart</a>
+        </div>
+        <div class="product-item">
+          <img src="product2.jpg" alt="Product 2">
+          <h3>Product 2</h3>
+          <p>$39.99</p>
+          <a href="#" class="btn">Add to Cart</a>
+        </div>
+        <div class="product-item">
+          <img src="product3.jpg" alt="Product 3">
+          <h3>Product 3</h3>
+          <p>$29.99</p>
+          <a href="#" class="btn">Add to Cart</a>
+        </div>
+      </div>
+      <a href="#" class="btn">View All Products</a>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <p>&copy; 2023 My Clothing Store. All rights reserved.</p>
+    </div>
+  </footer>
+
+</body>
+</html>
